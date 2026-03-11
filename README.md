6616efdbb476c5d726f219eb2b8b279dbff14c4c
Clelia (Astra) Bertelli <clelia@runllama.ai>
2026-03-11 16:55:25 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# No
+
+The 'no' command is the same as the 'yes' command, but it prints 'n'.
+
+## Installation
+
+With npm:
+
+```bash
+npm install -g @cle-does-things/no
+```
+
+With brew:
+
+```bash
+brew tap AstraBert/no-ts
+brew install no-ts
+```
---
+++ src/index.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+
+const VERSION = "0.1.0";
+
+function main(): void {
+  const firstArg = process.argv.at(2);
+  if (firstArg && firstArg === "--help") {
+    console.log(
+      "Prints 'n' to the console an infinite amount of time (equivalent to the 'yes' command).\nOptions:\n--version: Print the version of the command\n--help: Print the help message.",
+    );
+    return;
+  } else if (firstArg === "--version") {
+    console.log(`no - v${VERSION}`);
+    return;
+  }
+  while (true) {
+    console.log("n");
+  }
+}
+
+main();
